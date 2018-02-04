aa70977621fcb2967f4d4e1543fae8c34c935b46
manu <manu95dh@gmail.com>
2018-02-04 11:43:01 +0100

    ready

--- ansible/conf/wordpress/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x wp-cli.phar
-./wp-cli.phar core install --url=$1 --title=$2 --admin_user=$3 --admin_password=$4 --admin_email=wordpress@wordpress.com --path=/var/www/html/wordpress --allow-root
